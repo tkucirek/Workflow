55f3a8d4d69a8f6b9a3164611db177c685eca241
tkuci <tkuci@192.168.0.200>
2018-06-17 16:41:00 +0200

    First test

---
+++ src/main/java/org/camunda/bpm/getstarted/chargecard/ChargeCardWorker.java
@@ -0,0 +1,31 @@
+package org.camunda.bpm.getstarted.chargecard;
+
+import java.util.logging.Logger;
+
+import org.camunda.bpm.client.ExternalTaskClient;
+
+public class ChargeCardWorker {
+  private final static Logger LOGGER = Logger.getLogger(ChargeCardWorker.class.getName());
+
+  public static void main(String[] args) {
+    ExternalTaskClient client = ExternalTaskClient.create()
+        .baseUrl("http://localhost:8080/engine-rest")
+        .build();
+
+    // subscribe to an external task topic as specified in the process
+    client.subscribe("charge-card")
+        .lockDuration(1000) // the default lock duration is 20 seconds, but you can override this
+        .handler((externalTask, externalTaskService) -> {
+          // Put your business logic here
+
+          // Get a process variable
+          String item = (String) externalTask.getVariable("item");
+          Long amount = (Long) externalTask.getVariable("amount");
+          LOGGER.info("Charging credit card with an amount of '" + amount + "'€ for the item '" + item + "'...");
+
+          // Complete the task
+          externalTaskService.complete(externalTask);
+        })
+        .open();
+  }
+}
