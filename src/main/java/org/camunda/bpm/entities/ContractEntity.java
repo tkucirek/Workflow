07e201f1cdd6b46918a10f04c35bab1a0a9c1f63
hotpotgg <32904676+hotpotgg@users.noreply.github.com>
2018-06-29 16:30:32 +0200

    first

--- src/main/java/org/camunda/bpm/entities/ContractEntity.java
+++ src/main/java/org/camunda/bpm/entities/ContractEntity.java
@@ -1,9 +1,72 @@
 package org.camunda.bpm.entities;
 
-public class ContractEntity {
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+public class ContractEntity implements Serializable {
 	
+	private static  final long serialVersionUID = 1L;
+
+	  @Id
+	  @GeneratedValue
+	  protected Long id;
+
+	  @Version
+	  protected long version;
+	  
+
 	protected Long customerId;
+	protected Long vehicle_model;
+	protected Long number_of_vehicles;
+	protected int contractId;
 	protected double price;
 	protected double duration;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
+	}
+	public Long getVehicle_model() {
+		return vehicle_model;
+	}
+	public void setVehicle_model(Long vehicle_model) {
+		this.vehicle_model = vehicle_model;
+	}
+	public Long getNumber_of_vehicles() {
+		return number_of_vehicles;
+	}
+	public void setNumber_of_vehicles(Long number_of_vehicles) {
+		this.number_of_vehicles = number_of_vehicles;
+	}
+	public int getContractId() {
+		return contractId;
+	}
+	public void setContractId(int contractId) {
+		this.contractId = contractId;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public double getDuration() {
+		return duration;
+	}
+	public void setDuration(double duration) {
+		this.duration = duration;
+	}
+
 
 }
--- src/main/java/org/camunda/bpm/main/Contract.java
+++ src/main/java/org/camunda/bpm/main/Contract.java
@@ -1,8 +1,44 @@
 package org.camunda.bpm.main;
 import org.camunda.bpm.engine.*;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.entities.ContractEntity;
+import org.camunda.bpm.engine.cdi.jsf.TaskForm;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.io.IOException;
+import java.util.Map;
 
 public class Contract {
+	  private EntityManager entityManager;
+	  
+	public void PersistContract (DelegateExecution test) {
+		 // Create new order instance
+	    ContractEntity contractEntity = new ContractEntity();
+
+	    // Get all process variables
+	    Map<String, Object> variables = test.getVariables();
+
+	    // Set order attributes
+	    contractEntity.setCustomer((String) variables.get("customer"));
+	    contractEntity.setAddress((String) variables.get("address"));
+	    contractEntity.setPizza((String) variables.get("pizza"));
+
+	    // Persist order instance and flush. After the flush the
+	    // id of the order instance is set.
+	    entityManager.persist(contractEntity);
+	    entityManager.flush();
+
+	    // Remove no longer needed process variables
+	    test.removeVariables(variables.keySet());
+
+	    // Add newly created order id as process variable
+	    test.setVariable("orderId", contractEntity.getId());
+	}
 
 	public void CheckInformation (DelegateExecution test) {
 		
