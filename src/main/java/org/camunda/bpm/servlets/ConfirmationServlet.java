48078fc034420c2d294faf578a96ea133c36a3e8
hotpotgg <32904676+hotpotgg@users.noreply.github.com>
2018-07-07 01:29:56 +0200

    +write process id in db and read in confirmation servlet

--- src/main/java/org/camunda/bpm/servlets/ConfirmationServlet.java
+++ src/main/java/org/camunda/bpm/servlets/ConfirmationServlet.java
@@ -2,6 +2,11 @@ package org.camunda.bpm.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,10 +21,12 @@ import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.impl.util.json.JSONObject;
 import org.camunda.bpm.engine.runtime.MessageCorrelationResult;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.main.Customize;
 
 public class ConfirmationServlet extends HttpServlet {
 	int finalPrice;
 	String offertype;
+	String process_Id;
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
@@ -43,24 +50,54 @@ public class ConfirmationServlet extends HttpServlet {
 		map.put("customer_id", customer_id);
 		map.put("offer_id", offer_id);
 		
-		MessageCorrelationResult processInstance;
-		ProcessInstance processinstance;
-		String prozessid;
-		
-		
-		runtimeService.correlateMessage("contractConfirmed");
-		
+		try {
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		Connection connection = null;
+		try {
+			// create a database connection
+			connection = DriverManager.getConnection(Customize.databasepath);
+			Statement statement = connection.createStatement();
+			statement.setQueryTimeout(30);
+
+			ResultSet result = statement.executeQuery("SELECT Process_Id from Instance WHERE Bvis_Id="+customer_id);
 			
-		processinstance = processInstance.getProcessInstance();
-		prozessid = processinstance.getId();
+				 process_Id=result.getString("Process_Id");
+				 System.out.println(process_Id);
+		}
+		
+		catch (SQLException e) {
+			System.err.println(e.getMessage());
+		} finally {
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (SQLException e) {
+				System.err.println(e);
+			}
+		}
 		
 		
 		
+		runtimeService.createMessageCorrelation("receiveContractConfirmation").processInstanceId(process_Id).setVariable("lolotest", 5)
+		.correlateWithResult();
+		/*ProcessInstance processinstance;
+		String prozessid;
+		runtimeService.
+		MessageCorrelationResult result = runtimeService
+		        .createMessageCorrelation("contractConfirmed").setVariable("customer", "customer").correlateWithResult();
+		         //trigger instance where customer matche*/
+		
+		
+	
 		
 		
 		
-		
-		if(json.getLong("offer_id")== 1) {
+		/*if(json.getLong("offer_id")== 1) {
 			finalPrice = (int) runtimeService.getVariable(prozessid, "offerfull");
 			offertype = "Vollkasko";
 			
@@ -72,7 +109,7 @@ public class ConfirmationServlet extends HttpServlet {
 		runtimeService.setVariable(prozessid, "finalPrice", finalPrice);
 		
 		runtimeService.setVariable(prozessid, "offertype" , offertype);
-		
+		*/
 		
 	
 	}
