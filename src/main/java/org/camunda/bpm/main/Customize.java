2edaaf57938f28268cab87f5120a097a97e2621d
tkuci <tkuci@10.66.13.56>
2018-07-09 13:18:46 +0200

    +beginclaimservlet

--- src/main/java/org/camunda/bpm/main/Customize.java
+++ src/main/java/org/camunda/bpm/main/Customize.java
@@ -3,6 +3,6 @@ package org.camunda.bpm.main;
 public class Customize {
 
 	//public static String bvisURL = "http://10.67.7.228:8080/claim-0.0.1-SNAPSHOT";
-	public static String databasepath = "jdbc:sqlite:C:/Users/Felix Laptop/git/Workflow2/Datenbank2.db";
+	public static String databasepath = "jdbc:sqlite:C:/Users/tkuci/git/Workflow/Datenbank2.db";
 
 }
---
+++ src/main/java/org/camunda/bpm/servlets/BeginClaimServlet.java
@@ -0,0 +1,65 @@
+package org.camunda.bpm.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngines;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.impl.util.json.JSONObject;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.main.Customize;
+import org.camunda.bpm.messages.InsuranceOffering;
+
+public class BeginClaimServlet extends HttpServlet {
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+		RuntimeService runtimeService = processEngine.getRuntimeService();
+		PrintWriter out = response.getWriter();
+		
+
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		
+		String jsonString = IOUtils.toString(request.getInputStream());
+
+		JSONObject json = new JSONObject(jsonString); 
+		
+		String customer_id = String.valueOf(json.getLong("customer_id")) ;
+		String damage_classification = String.valueOf(json.getLong("damage_classification"));
+		
+		
+		map.put("customer_id", customer_id);
+		map.put("damage_classification", damage_classification);
+		
+		ProcessInstance processInstance;
+		
+		processInstance = runtimeService.startProcessInstanceByMessage("instantiationMessageClaim", map);
+		String prozessid = processInstance.getId();
+		
+		String deductible_Amount = (String) runtimeService.getVariable(prozessid, "deductible_Amount");
+		
+		
+		out.print(deductible_Amount);
+		
+		
+}
+}
